0348f81c33bb7141b212a80617c9d460be1356ea
Dominik Lewandowski <d.lewandowski@mycit.ie>
2019-12-13 13:00:55 +0000

    first success at executing attempt.py

--- DataGathering.py
+++ DataGathering.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 
 # URL we are looking for
 url = "https://www.donedeal.ie/cars/Ford/C-MAX?start=1"
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 response = requests.get(url, headers=headers)
 
 queryURLS = []
@@ -14,44 +15,41 @@ queryURLS = []
 soup = BeautifulSoup(response.content, 'html.parser')
 for tag in soup.findAll('a', href=True, class_="card__link"):
     queryURLS.append(tag['href'])
-#print(queryURLS)
+# print(queryURLS)
 
-count=0
-#Pull data from each of the pages
+count = 0
+# Pull data from each of the pages
 for url in queryURLS:
     print(url)
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
-    #file = open("data/car"+str(count)+".html","w")
+    # file = open("data/car"+str(count)+".html","w")
     count += 1
 
-    #title = soup.findAll("h1", {"itemprop":"name"})
-    #print(title)
+    # title = soup.findAll("h1", {"itemprop":"name"})
+    # print(title)
 
-    #content = soup.prettify()
-    #print(content)
-    #file.write(content)
-    #file.close()
+    # content = soup.prettify()
+    # print(content)
+    # file.write(content)
+    # file.close()
 
-
-
-    #print(soup.prettify())
+    # print(soup.prettify())
 
     title = soup.findAll("title")
     print(title)
-    #price = soup.findAll("span", {"class" : "price ng-binding"})
-    #print(price)
-    #county = soup.findAll('span', class_="county-disp ng-binding")
+    # price = soup.findAll("span", {"class" : "price ng-binding"})
+    # print(price)
+    # county = soup.findAll('span', class_="county-disp ng-binding")
 
-    #print(price)
-    #print(county)
+    # print(price)
+    # print(county)
 
-
-    #main-content page-row ng-scope
-    #< h1 itemprop = "name" ng-bind-html = "adView.ad.header" class ="ng-binding" > Ford C-Max < / h1 >
-    #<span class="county-disp ng-binding">Offaly</span>
-    #<span class ="price ng-binding" > 999 < / span >
+    # main-content page-row ng-scope
+    # < h1 itemprop = "name" ng-bind-html = "adView.ad.header" class ="ng-binding" > Ford C-Max < / h1 >
+    # <span class="county-disp ng-binding">Offaly</span>
+    # <span class ="price ng-binding" > 999 < / span >
 
 #    class ="cad-content divider"
 
-#class="cad-info-container space-top-10"
+# class="cad-info-container space-top-10"
