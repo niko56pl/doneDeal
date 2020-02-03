f6f86a94c8eb6b39bddf39009e9fca4d8c93d207
Dominik Lewandowski <d.lewandowski@mycit.ie>
2020-02-03 20:36:22 +0000

    Second commit. Problem with getting a result when trying to get just the prices.

--- DataGathering.py
+++ DataGathering.py
@@ -1,9 +1,13 @@
 import requests
 import re
+import locale
 from bs4 import BeautifulSoup
 
+#locale.setlocale( locale.LC_ALL, 'en_US.UTF-8' )
+#d = {'€':1,'£':1.2}
+
 # URL we are looking for
-url = "https://www.donedeal.ie/cars/Ford/C-MAX?"
+url = "https://www.donedeal.ie/cars/Ford/C-MAX"
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 response = requests.get(url, headers=headers)
@@ -12,16 +16,31 @@ response = requests.get(url, headers=headers)
 # PULL THE 1st card-collection from the page as there is a 2nd with advertisments.
 # More info on regex to be found below
 # https://stackoverflow.com/questions/499345/regular-expression-to-extract-url-from-an-html-link
-soup = BeautifulSoup(response.content, 'html.parser')
 
-price_list = soup.findAll(class_='card__price')
+soup = BeautifulSoup(response.text, 'html.parser')
+
+#price_list = soup.findAll(re.compile('/-->\d +,\d + <!--/g'), class_='card__price')
+#price_list = soup.findAll('card__price', ex= re.compile("-->\d +,\d + <!--"))
+
+price_list= soup.findAll('card__price', {"class": re.compile("-->\d +,\d + <!--")})
+
+len(price_list)
+
 for price in price_list:
-    #prices = price.contents[0]
-    #prices = re.search("")
-    #soup.findAll(re.compile("(...)"))
+    prices = price.contents[0]
+    regex_pattern = prices.compile("\d+")
+    price_figure = prices
+    result = "".join(regex_pattern.findall(price_figure))
+    len(price_list)
+    print(prices + "\n")
+    print(result)
 
-    print(prices)
 
+#for item in soup.get_text().split():
+#    currency = item[0]
+#    value = locale.atof(item[1:])
+#    k = d[currency]
+#    print(currency,value,k*value)
 
 #for tag in soup.findAll('a', href=True, class_="card__link"):
 #    queryURLS.append(tag['href'])
